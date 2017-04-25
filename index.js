ab796c7ba961d15dee4de3a4e69afc41af9da019
Teerasej <teerasej@gmail.com>
2017-04-26 00:23:21 +0700

    Say hi with a user name

--- index.js
+++ index.js
@@ -22,64 +22,10 @@ var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
 
-//Intent dialog
-// var luisUrl = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/109f950e-11c6-462a-b84c-f3454a1fc79e?subscription-key=61fe645b6056470cba59a88b0d6d927d&verbose=true';
-// var luisRecognizer = new builder.LuisRecognizer(luisUrl);
-// var intentDialog = new builder.IntentDialog({recognizers: [luisRecognizer]});
-
 var intentDialog = new builder.IntentDialog();
 
 //Root dialog
-bot.dialog('/', intentDialog);
-intentDialog.onDefault(builder.DialogAction.send('Sorry, I didn\'t understand that.'));
-intentDialog.onBegin(function(session,args, next){
-    if(session.userData.name){
-        
-        session.endDialog('Hi ' +  session.userData.name);
-    } else {
-        session.endDialog('Hi Guy!');
-    }
-})
-intentDialog.matches(/^Greeting/i,'/greetingDialog');
-intentDialog.matches(/^Size/i, '/sizeDialog');
-intentDialog.matches(/^Distance/i, '/distanceDialog');
-intentDialog.matches(/^Life/i, '/lifeDialog');
+bot.dialog('/', session => {
+    session.send('Hi, I\'m a bot, say something...,' + session.message.user.name);
 
-bot.dialog('/greetingDialog', 
-    function(session)
-    {
-        session.endDialog('Hi! I\'m a droid from Mars. Ask me questions you have about Mars!');
-    }
-)
-
-bot.dialog('/sizeDialog',
-    function(session)
-    {
-        session.endDialog('Mars has a radius of 3,390km, compared to Earth\'s radius which is 6,371km');
-    }
-)
-
-bot.dialog('/distanceDialog',
-    function(session)
-    {
-        session.endDialog('Mars is pretty far away from your home planet, Earth. On average, the distance is 225 million km.');
-    }
-)
-
-bot.dialog('/lifeDialog', 
-    function(session)
-    {
-        session.endDialog('Yes, there is life on Mars although not yet discovered by primitive humans.');
-    }
-)
-
-
-// =========================================================
-// Bots Dialogs 
-// =========================================================
-// This is called the root dialog. It is the first point of entry for any message the bot receives
-// bot.dialog('/', function (session) {
-//     // Send 'hello world' to the user
-//     session.send("Hello Master");
-//     session.sendTyping();
-// });
+});
